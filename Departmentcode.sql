cb8e5f64d8eb673727bc8f4970a3b61578fbddda
Abhirami p kumar_kinghuzzzz <97221313+AbhiramiP123@users.noreply.github.com>
2022-06-01 14:51:37 +0530

    department code

---
+++ Departmentcode.sql
@@ -0,0 +1,17 @@
+INSERT INTO Department(Dnumber,Mgr_ssn,mgr_start_date)
+VALUES(11,101,'10-jan-2022');
+
+INSERT INTO Department(Dnumber,Mgr_ssn,mgr_start_date)
+VALUES(13,103,'17-feb-2022');
+
+INSERT INTO Department(Dnumber,Mgr_ssn,mgr_start_date)
+VALUES(12,105,'07-march-2022');
+
+INSERT INTO Department(Dnumber,Mgr_ssn,mgr_start_date)
+VALUES(16,102,'27-march-2022');
+
+INSERT INTO Department(Dnumber,Mgr_ssn,mgr_start_date)
+VALUES(15,108,'27-april-2022');
+
+INSERT INTO Department(Dnumber,Mgr_ssn,mgr_start_date)
+VALUES(18,106,'28-may-2022');
